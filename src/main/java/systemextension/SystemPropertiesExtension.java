574cc35e1af5e642e0a053e3ee085a4cae0ba5e3
Josh Windels <joshuawindels@gmail.com>
2020-04-04 00:44:37 +0200

    add JUnit5 API dependency

---
+++ src/main/java/systemextension/SystemPropertiesExtension.java
@@ -0,0 +1,31 @@
+package extension;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.Properties;
+
+import static java.lang.System.getProperties;
+import static java.lang.System.setProperties;
+
+public class SystemPropertiesExtension implements BeforeEachCallback, AfterEachCallback {
+
+  private Properties originalProperties;
+
+  public void beforeEach(final ExtensionContext extensionContext) throws Exception {
+    originalProperties = getProperties();
+    setProperties(copyOf(originalProperties));
+  }
+
+  public void afterEach(final ExtensionContext extensionContext) throws Exception {
+    setProperties(originalProperties);
+  }
+
+  private Properties copyOf(Properties source) {
+    Properties copy = new Properties();
+    copy.putAll(source);
+    return copy;
+  }
+
+}
